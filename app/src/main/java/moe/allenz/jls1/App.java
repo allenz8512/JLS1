b5ed2938078633f5e699fe638bd6b021e8ecc48a
allenz8512 <allenz8512@gmail.com>
2014-11-24 18:03:36 +0800

    add custom application class

---
+++ app/src/main/java/moe/allenz/jls1/App.java
@@ -0,0 +1,15 @@
+package moe.allenz.jls1;
+
+import android.app.Application;
+
+/**
+ * Custom application instance of this app.
+ * <p>
+ * Created by Allenz on 2014/11/24.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+    }
+}
